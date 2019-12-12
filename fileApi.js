69a723fccf1bfac48e7f940f4931d276bee0cfc3
nikitakonan <nikitakonan1991@gmail.com>
2019-12-12 13:07:53 +0300

    Added -register and -status apis

---
+++ fileApi.js
@@ -0,0 +1,34 @@
+const fs = require('fs');
+
+const getUsers = () => {
+    return new Promise((resolve, reject) => {
+        fs.readFile('./users.json', (err, data) => {
+            if (err) {
+                reject(err);
+            }
+            try {
+                const users = JSON.parse(data);
+                resolve(users);
+            } catch (e) {
+                reject(e);
+            }
+        });
+    });
+}
+
+const setUsers = (users) => {
+    return new Promise((resolve, reject) => {
+        const data = JSON.stringify(users, null, 2);
+        fs.writeFile('./users.json', data, err => {
+            if (err) {
+                return reject(err);
+            }
+            resolve();
+        });
+    });
+}
+
+module.exports = {
+    getUsers,
+    setUsers
+}
--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 require('dotenv').config();
 const express = require('express');
 const ViberBot = require('viber-bot').Bot;
-const BotEvents = require('viber-bot').Events;
+const TextMessage = require('viber-bot').Message.Text;
+const { getUsers, setUsers } = require('./fileApi');
 
 const authToken = process.env.PRIVATE_TOKEN;
 const name = process.env.NAME;
@@ -12,23 +13,62 @@ const webhookPath = '/viber/webhook';
 
 const app = express();
 
+app.set('view engine', 'pug');
+
 const bot = new ViberBot({
     authToken,
     name,
     avatar
 });
 
-bot.on(BotEvents.MESSAGE_RECEIVED, (message, response) => {
-    response.send(message);
+bot.onTextMessage(/^register$/i, (message, response) => {
+    const { id, name } = response.userProfile;
+
+    getUsers()
+        .then(users => {
+            const existing = users.find(u => u.id === id);
+            if (existing) {
+                response.send(new TextMessage(`Вы уже зарегистрированы 🤦`));
+                return true;
+            }
+
+            users.push({ id, name });
+            return setUsers(users);
+        })
+        .then((skip) => {
+            if (skip) {
+                return;
+            }
+            response.send(new TextMessage(`${name}, Вы успешно зарегистрированы 😀`));
+        })
+        .catch(() => {
+            response.send(new TextMessage(`Что-то пошло не так 😟`));
+        });
+});
+
+bot.onTextMessage(/^status$/i, (message, response) => {
+    const { id, name } = response.userProfile;
+    getUsers()
+        .then(users => {
+            const user = users.find(u => u.id === id);
+            const txt = user ?
+                `${name}, Вы успешно зарегистрированы, ожидайте других пользователей 😆` :
+                `Для участия введите сообщение: register 😅`;
+            const msg = new TextMessage(txt);
+            response.send(msg);
+        })
+        .catch(() => {
+            response.send(new TextMessage(`Что-то пошло не так 😟`));
+        });
 });
 
 app.get('/', (req, res) => {
-    res.send(`<html>
-    <body>
-        <h1>Hello</h1>
-    </body>
-    </html>`)
-})
+    res.render('users', {
+        title: 'Secret Santa',
+        message: 'Привет',
+        users: registeredUsers
+    });
+});
 
 app.use(webhookPath, bot.middleware());
 
